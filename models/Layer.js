01aac2b68d2be25e6e3756bb1ef0a6900d24eebf
Edgar Dimas Isaadrazak <edgar.dimas.ir@gmail.com>
2024-02-11 12:44:02 +0700

    success change into mvc

--- models/Layer.js
+++ models/Layer.js
@@ -1,16 +1,19 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
+const ground = new Image();
+const city = new Image();
+const clouds = new Image();
+const sky = new Image();
+
 ground.src = "../views/assets/ground.png";
 sky.src = "../views/assets/sky.png";
 clouds.src = "../views/assets/clouds.png";
 city.src = "../views/assets/city.png";
 
-const canvas = document.getElementById("canvas1");
-const ctx = canvas.getContext("2d");
-const city = new Image();
-const clouds = new Image();
-const sky = new Image();
 let gameSpeed = 10;
 
-module.exports = class Layer {
+class Layer {
   constructor(image, speedModifier) {
     this.x = 0; // each object will have horizontal x coordinate that starts at position 0
     this.y = 0; // each object will have y coordinate 0
@@ -38,11 +41,13 @@ module.exports = class Layer {
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
   }
-};
+}
 
 const Ground = new Layer(ground, 0.5); // this is instance
 const Sky = new Layer(sky, 0.5);
 const Clouds = new Layer(clouds, 0.2);
 const City = new Layer(city, 0.3);
 
-export default { Ground, Sky, Clouds, City };
+export { Ground, Sky, Clouds, City };
+
+export default Layer;
--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-import { Ground, Sky, Clouds, City } from "./models/Layer";
+import { Ground, Sky, Clouds, City } from "./models/Layer.js";
 
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 800); // the same width in the css
 const CANVAS_HEIGHT = (canvas.height = 700);
 const gameObjects = [Sky, Clouds, City, Ground];
-const ground = new Image();
-const input = new InputHandler();
+// const ground = new Image();
+// const input = new InputHandler();
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
